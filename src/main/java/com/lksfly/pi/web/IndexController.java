daa709b04b17152f4b253aa352fbbb120b9c1976
lksfly <bonobonoks@naver.com>
2022-02-14 20:30:41 +0900

    view resolver 적용

--- src/main/java/com/lksfly/pi/web/IndexController.java
+++ src/main/java/com/lksfly/pi/web/IndexController.java
@@ -12,9 +12,9 @@ public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("===========================");
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("data","Hello Spring MVC~");
-		mv.setViewName("index.jsp");
+		//mv.setViewName("/WEB-INF/views/index.jsp");
 		
 		return mv;
 	}
