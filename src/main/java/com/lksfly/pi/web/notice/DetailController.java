86bf0140b81b887c5328ff470dfb024b66364459
leekwonseok <kslee@bizdata.kr>
2022-02-22 08:07:38 +0900

    공지사항 화면 생성 	- ListController, DetailController 생성 	- list.jsp , detail.jsp 생성 	- dispatcher-servlet.xml에 url경로 설정

--- src/main/java/com/lksfly/pi/web/notice/DetailController.java
+++ src/main/java/com/lksfly/pi/web/notice/DetailController.java
@@ -1,4 +1,4 @@
-package com.lksfly.pi.web;
+package com.lksfly.pi.web.notice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+public class DetailController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("===========================!");
-		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("data","Hello Spring MVC~");
-		//mv.setViewName("/WEB-INF/views/index.jsp");
-		
+		System.out.println("Detail========");
+		ModelAndView mv = new ModelAndView("notice/detail");
+		//mv.setViewName("/WEB-INF/views/index.jsp");				
 		return mv;
 	}
-	
+
 }
--- src/main/java/com/lksfly/pi/web/notice/ListController.java
+++ src/main/java/com/lksfly/pi/web/notice/ListController.java
@@ -1,4 +1,4 @@
-package com.lksfly.pi.web;
+package com.lksfly.pi.web.notice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+public class ListController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("===========================!");
-		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("data","Hello Spring MVC~");
-		//mv.setViewName("/WEB-INF/views/index.jsp");
-		
+		System.out.println("List========");
+		ModelAndView mv = new ModelAndView("notice/list");
+		//mv.setViewName("/WEB-INF/views/index.jsp");				
 		return mv;
 	}
-	
+
 }
