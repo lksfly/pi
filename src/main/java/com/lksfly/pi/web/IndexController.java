0f8008be270f622e9e72fbc04f6f5be66efc8f1c
lksfly <bonobonoks@naver.com>
2022-03-01 18:37:13 +0900

    root페이지(index.jsp)들을 위한 layout 페이지 만들기 	- tiles 설정시 * 가 아닌 root.* 로 해주

--- src/main/java/com/lksfly/pi/web/IndexController.java
+++ src/main/java/com/lksfly/pi/web/IndexController.java
@@ -12,7 +12,7 @@ public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("===========================!");
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("root.index");
 		mv.addObject("data","Hello Spring MVC~");
 		//mv.setViewName("index");
 		
